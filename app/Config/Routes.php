015bdd924c51fe414b6cc051886ad321b846a338
scottminterlq <scott.minter@leasequery.com>
2022-08-31 09:36:29 -0400

    added get route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,6 +37,7 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->get('/health', 'Health::index');
+$routes->get('/mirror', 'Mirror::index');
 $routes->post('/mirror', 'Mirror::index');
 $routes->put('/mirror', 'Mirror::index');
 $routes->delete('/mirror', 'Mirror::delete');
